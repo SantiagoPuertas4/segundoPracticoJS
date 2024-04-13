6912506a85af3525450b432ab77ac031c47a0be7
Santiago Puertas <santidoors13@gmail.com>
2024-04-13 03:18:47 -0300

    Primer ejercicio terminado

---
+++ 1/js/ejercicio1.js
@@ -0,0 +1,19 @@
+/*
+1- Escribir un programa que solicite la edad y si es mayor de 18 años mostrar un mensaje que ya puede conducir, si la edad ingresada no es un número válido indicarlo en un mensaje.
+ */
+
+const edad = Number(prompt("Ingresa tu edad"));
+
+if (isNaN(edad)) {
+  alert("No es un numero");
+} else {
+  if (edad <= 0) {
+    alert("No es un numero valido");
+  } else if (edad > 100) {
+    alert("Vuelva a la tumba");
+  } else if (edad >= 18) {
+    alert("Puede conducir y embriagarse");
+  } else {
+    alert("No puede conducir ni embriagarse");
+  }
+}
