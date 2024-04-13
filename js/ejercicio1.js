dbd717a4cf063978b344948970386a6bf896e92c
Santiago Puertas <santidoors13@gmail.com>
2024-04-13 04:22:14 -0300

    Agregado validacion de null al primer ejercicio

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -2,9 +2,11 @@
 1- Escribir un programa que solicite la edad y si es mayor de 18 años mostrar un mensaje que ya puede conducir, si la edad ingresada no es un número válido indicarlo en un mensaje.
  */
 
-const edad = Number(prompt("Ingresa tu edad"));
+const edad = prompt("Ingresa tu edad");
 
-if (isNaN(edad)) {
+if (edad === null) {
+  alert("No ingreso nada");
+} else if (isNaN(edad)) {
   alert("No es un numero");
 } else {
   if (edad <= 0) {
