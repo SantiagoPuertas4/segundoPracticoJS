e0abbb5eed2db8533648ca93fd2fdf8bde7b0232
Santiago Puertas <santidoors13@gmail.com>
2024-04-14 23:36:32 -0300

    Terminado ejercicio 14

---
+++ js/ejercicio14.js
@@ -0,0 +1,28 @@
+// 14- Realiza un script que pida una cadena de texto y lo muestre poniendo el signo – entre cada carácter sin usar el método replace.
+// Por ejemplo, si tecleo “hola qué tal”, deberá salir “h-o-l-a- -q-u-e- -t-a-l”.
+
+let cadena = "hola que tal";
+let cadenaRes = "";
+
+do {
+  cadena = prompt("Ingresa una cadena de texto");
+
+  if (cadena === null) {
+    alert("No se ingreso nada");
+  } else {
+    break;
+  }
+} while (true);
+
+cadena = cadena.trim();
+
+for (i = 0; i < cadena.length; i++) {
+  if (i === 0) {
+    cadenaRes = cadena.charAt(i);
+  } else {
+    cadenaRes = cadenaRes + "-" + cadena.charAt(i);
+  }
+}
+
+alert(cadena);
+alert(cadenaRes);
