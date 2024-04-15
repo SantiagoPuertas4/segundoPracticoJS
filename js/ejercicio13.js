b988dc70482c8dc2cbbb5c2211e1dd213b3b2ba6
Santiago Puertas <santidoors13@gmail.com>
2024-04-15 00:12:34 -0300

    Terminado ejercicio 13

---
+++ js/ejercicio13.js
@@ -0,0 +1,20 @@
+//13- Realiza un script que pida un texto y lo muestre en mayúsculas.
+
+let cadena = "hola que tal";
+let cadenaMayus = "";
+
+do {
+  cadena = prompt("Ingresa una cadena de texto");
+
+  if (cadena === null) {
+    alert("No se ingreso nada");
+  } else {
+    break;
+  }
+} while (true);
+
+cadena = cadena.trim();
+
+cadenaMayus = cadena.toUpperCase();
+
+alert(cadenaMayus);
